7d639daea97169ae33508f42610d2849c09ced95
ShubhamSattigeri1 <sattigeri.shubham@outlook.com>
2025-03-10 23:54:13 +0530

    game op

--- screen.py
+++ screen.py
@@ -1,11 +1,18 @@
 import streamlit as st
-from back import display_info, form
-#st.write("Here are the patients yet to arrive....")
-def display_info():
-    name, age, phone = form()
-    st.write("Here are the patients yet to arrive....")
-    st.write("Name: ", name)
-    st.write("Age: ", age)
-    st.write("Phone: ", phone)
+from back import form, submit
 
-display_info()
+def display_info():
+    if 'form_submitted' in st.session_state and st.session_state['form_submitted']:
+        st.write("Here are the patients yet to arrive....")
+        st.write("Name: ", st.session_state['name'])
+        st.write("Age: ", st.session_state['age'])
+        st.write("Phone: ", st.session_state['phone'])
+    else:
+        st.write("No information available. Please fill out the form first.")
+
+# Main logic
+if 'form_submitted' not in st.session_state:
+    count, name, age, phone = form()
+    submit(count)
+else:
+    display_info()
